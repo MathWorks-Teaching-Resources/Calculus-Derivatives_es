9729156398b7133286d9eb3b366be008a79dbde0
Emma Smith Zbarsky <esmithzb@mathworks.com>
2024-10-07 16:04:07 -0400

    Fix test failures
    1) Correct references to TarjetasDeCalculo from TarjetasCalculo
    2) Add "Tierra" in place of "Earth" in drop-down menu

--- SoftwareTests/FunctionTests.m
+++ SoftwareTests/FunctionTests.m
@@ -1,8 +1,19 @@
+% Run these tests with 
+% runMyTests
+% OR
+% results = runtests(tLibrary)
+% table(results)
 classdef FunctionTests < matlab.unittest.TestCase
 
-    % https://www.mathworks.com/help/matlab/matlab_prog/use-parameters-in-class-based-tests.html
-
-    methods(Test)    
+    methods(Test)
+        function RunShowTaylor(testCase)
+            % Test that correct answers are marked correct
+            T = ShowTaylor(@cos,3,[-2,2],0);
+            % T should be 1-x^2/2
+            syms x
+            E = str2sym("1-x^2/2");
+            verifyTrue(testCase,isAlways(T==E))
+        end
 
     end % methods
 
